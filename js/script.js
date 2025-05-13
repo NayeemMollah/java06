93157fb3cf50dc5292bc6e3cf214ec614fa328a9
Nayeem Mollah <nayeemmollah22279@gmail.com>
2025-05-13 02:50:47 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let reviewbox = document.getElementById("reviewbox")
+let btn = document.querySelector(".btn")
+
+btn.addEventListener("click", function () {
+ console.log(reviewbox.value);
+     
+})
